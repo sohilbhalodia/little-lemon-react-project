d8698d998ff1251ed6fde4f743c043d674ededb6
Sohil Bhalodia <sohil.bhalodia@hotmail.com>
2023-10-23 12:57:32 -0400

    Final Project Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {  render, screen } from '@testing-library/react';
+import Booking from './booking';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('Booking form', () => {
+  
+  render(
+    <BrowserRouter>
+      <Booking />
+    </BrowserRouter>);
+    const firstname = screen.getByLabelText(/First Name/i);
+    expect(firstname).toBeRequired();
+
+    const lastName = screen.getByLabelText(/Last Name/i);
+    expect(lastName).toBeRequired();
+
+    const Email = screen.getByLabelText(/Email/i);
+    expect(Email).toBeRequired();
+
+    const choosedate = screen.getByLabelText(/Choose Date/i);
+    expect(choosedate).toBeRequired();
+
+    const ChooseTime = screen.getByLabelText(/Choose Time/i);
+    expect(ChooseTime).toBeRequired();
+
+    const numberofguest = screen.getByLabelText(/Number of Guest/i);
+    expect(numberofguest).toBeRequired();
+
+    expect(firstname).toHaveValue(value, {''});
+
+    const makeabooking = screen.getByRole('button', {name:'Make a booking'});
+    expect(makeabooking).toBeDisabled();
+
 });
